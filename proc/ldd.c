cbb54e5861748bc96a1bae2f08a80eed50d5c27e
Thamarai-multipass <mahasel.1969@gmail.com>
2025-10-07 11:02:32 +0530

    printing to user space

--- proc/ldd.c
+++ proc/ldd.c
@@ -11,7 +11,28 @@ static struct proc_dir_entry *custom_proc_node;
 
 static ssize_t	lotus_read(struct file* fileptr, char  *user_space_buf, size_t count, loff_t* offset){
     printk("lotus_read");
-    return 0;
+
+    /* need to print or send to user space beacause this printk is only visible in kernel log buffer 
+    
+    1. user_space_buf is the one of the buff where we will send the message so that it can be printed in the console
+    2. copy_to_user is an api which helps to copy the message to the user buffer
+    3. cat excepts number bytes to be printed on the console if that is 0 then EOF no output 
+    4. we need to make use of offset which is important as it will be usefull to return 0 to cat so that it can stop.
+    
+    */
+
+    char message[] = "Ack!\n";
+    size_t str_len = strlen(message);
+
+    int result;
+
+    if ( *offset >= str_len) return 0;
+
+    result = copy_to_user(user_space_buf, message, str_len);
+    *offset += str_len;
+    printk("Count : %ld", count);
+
+    return str_len;
 }
 
 
