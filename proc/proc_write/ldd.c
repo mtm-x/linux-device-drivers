a289909c6aad1217ae8baa83cc8bd65f4a28b02e
Thamarai-multipass <mahasel.1969@gmail.com>
2025-10-12 11:03:03 +0530

    update comments

--- proc/proc_write/ldd.c
+++ proc/proc_write/ldd.c
@@ -2,7 +2,13 @@
 #include <linux/module.h>
 #include <linux/proc_fs.h>
 
-#define PROCFS_MAX_SIZE 10
+/*
+ *  PROCFS_MAX_SIZE is the maximum buffer size allocated for message if the message size is creater than this 
+ *  it leads to system failure and buffer overflow
+ *
+ */
+
+#define PROCFS_MAX_SIZE 1024
 
 MODULE_AUTHOR("Thamaraimanalan M");
 MODULE_LICENSE("GPL");
@@ -16,15 +22,15 @@ static ssize_t	lotus_read(struct file* fileptr, char  *user_space_buf, size_t co
     printk("lotus_read");
 
     /* 
-    
-    need to print or send to user space beacause this printk is only visible in kernel log buffer 
-    
-    1. user_space_buf is the one of the buff where we will send the message so that it can be printed in the console
-    2. copy_to_user is an api which helps to copy the message to the user buffer
-    3. cat excepts number bytes to be printed on the console if that is 0 then EOF no output 
-    4. we need to make use of offset which is important as it will be usefull to return 0 to cat so that it can stop.
-    
-    */
+     *
+     * need to print or send to user space beacause this printk is only visible in kernel log buffer 
+     *
+     * 1. user_space_buf is the one of the buff where we will send the message so that it can be printed in the console
+     * 2. copy_to_user is an api which helps to copy the message to the user buffer
+     * 3. cat excepts number bytes to be printed on the console if that is 0 then EOF no output 
+     * 4. we need to make use of offset which is important as it will be usefull to return 0 to cat so that it can stop.
+     *
+     */
     
     size_t str_len = strlen(message);
     int result;
@@ -37,21 +43,25 @@ static ssize_t	lotus_read(struct file* fileptr, char  *user_space_buf, size_t co
     *offset += str_len;
     printk("Count : %ld", count);
 
-    return str_len;
+    return str_len; 
 }
 
 static ssize_t	lotus_write(struct file* fileptr, const char  *user_space_buf, size_t count, loff_t* offset){
 
     printk("lotus_write");
+    /* Count --> it stores the number of bytes that written into the driver including NULL */
+    printk(KERN_INFO "Count: %ld ", count);
 
-    size_t procfs_buffer_size = count;
+    size_t procfs_buffer_size = count; /* Minimum Buffer size required to store the value */ 
     
 	if (procfs_buffer_size > PROCFS_MAX_SIZE ) {
-		procfs_buffer_size = PROCFS_MAX_SIZE;
+		procfs_buffer_size = PROCFS_MAX_SIZE; 
 	}
+
+    printk("procfs_buffer_size %ld", procfs_buffer_size);
 	
-	/* write data to the buffer */
-	if ( copy_from_user(message, user_space_buf, procfs_buffer_size) ) {
+	/* write data to the buffer if success returns 0 */
+	if (copy_from_user(message, user_space_buf, procfs_buffer_size)) {
 		return -EFAULT;
 	}
 	
@@ -71,9 +81,7 @@ static int lotus_module_init(void){
 
     printk("Init - entry");
 
-    /*const struct proc_ops *proc_ops cannot be NULL or else it will give some scary looking dmesg */
-
-    /*
+    /*const struct proc_ops *proc_ops cannot be NULL or else it will give some scary looking dmesg 
     
     struct proc_dir_entry *proc_create(const char *name, 
                                        umode_t mode,
@@ -95,10 +103,9 @@ static int lotus_module_init(void){
 static void lotus_module_exit(void){
 
     printk("exit - entry");
-
     proc_remove(custom_proc_node);
-
     printk("exit - exit");
+
 }
 
 module_init(lotus_module_init);
